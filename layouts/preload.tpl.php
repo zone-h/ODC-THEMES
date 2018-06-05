7ae3a53e24b8dedfb8355ffa0653a545e09479a5
orapakat <contact@orapakat.com>
2018-06-05 14:41:09 +0700

    upload odc 18.06.01

---
+++ layouts/preload.tpl.php
@@ -0,0 +1,22 @@
+<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?> 		
+		<div id="preloader">
+          <div class="preloader-position"> 
+            <img src="<?php echo THEMES_PATH.$this->theme_folder.'/'.$this->theme.'/'; ?>assets/img/logo-colored.png" alt="logo" >
+            <div class="progress">
+              <div class="indeterminate"></div>
+            </div>
+          </div>
+        </div>
+        <!-- <div id="preloader" class="preloader-wrapper big active">
+	      <div class="spinner-layer spinner-blue">
+	        <div class="circle-clipper left">
+	          <div class="circle"></div>
+	        </div><div class="gap-patch">
+	          <div class="circle"></div>
+	        </div><div class="circle-clipper right">
+	          <div class="circle"></div>
+	        </div>
+	      </div>
+
+	    </div> -->
+        
